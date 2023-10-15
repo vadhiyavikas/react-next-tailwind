676c9130f4c514fc5c41fcd9d5b7c6b893445121
vikas <vadhiyavikash@gamil.com>
2023-10-15 12:35:08 +0530

    -layout

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Landing() {
     return(
-        <div>
+        <div className="text-5xl text-black">
             Landing
         </div>
     )
